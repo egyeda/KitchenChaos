fe227f90603a0c09e53851db49bea6f205a485ad
Egyed Ádám <adam.egyed19@gmail.com>
2025-07-20 10:22:09 +0300

    delivery counter initial basic implementation without further functionality

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+	public override void Interact(Player player)
+	{
+		if (player.HasKitchenObject()) {
+			if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+			{
+				player.GetKitchenObject().DestroySelf();
+			}
+		}
+	}
+}
